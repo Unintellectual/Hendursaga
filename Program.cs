6aae02c505dc30552dad0169952d216709ebc4e3
Noah Victoriano <noahvictoriano0@gmail.com>
2025-03-19 20:49:27 +0800

    im failing in life

--- Program.cs
+++ Program.cs
@@ -6,25 +6,18 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
-// ✅ Move this before builder.Build();
-builder.Services.AddHttpClient("BackendClient", client =>
-{
-    client.BaseAddress = new Uri("http://localhost:5185"); // Force HTTP
-});
-
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
-app.MapGet("/api/hello", () => "Hello from the backend!");
-
 app.UseHttpsRedirection();
+
 app.UseStaticFiles();
 app.UseAntiforgery();
 
